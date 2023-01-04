7d9dff0f2022d1b52c4f04c320dd2ac5862cb958
PhyuThantHtun <phyuthant8803@gmail.com>
2023-01-04 21:47:45 +0630

    portfolio

---
+++ main.js
@@ -0,0 +1,44 @@
+import './style.scss'
+import Typed from 'typed.js';
+import ScrollReveal from 'scrollreveal';
+
+
+let options = {
+    strings: ['Phyu Thant Htun...', 'Frontend Developer...'],
+    typeSpeed: 30,
+    loop:true,
+    backDelay:2000,
+    backSpeed:30
+  };
+  
+  let typed = new Typed('.element', options);
+
+  let toDown = {
+    distance: '50px',
+    origin: 'top',
+    interval : 300,
+    duration: 800
+
+};
+
+ScrollReveal().reveal('.to-down', toDown);
+
+let toRight = {
+  distance: '50px',
+  origin: 'left',
+  interval : 300,
+  duration: 800
+
+};
+
+ScrollReveal().reveal('.to-right', toRight);
+
+let toLeft = {
+  distance: '50px',
+  origin: 'right',
+  interval : 300,
+  duration: 800
+
+};
+
+ScrollReveal().reveal('.to-left', toLeft);
